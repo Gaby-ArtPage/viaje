024e5573ac3f4c0e003eb5f90f632529914f4424
Gabriela Constanzo <gconstanzom@gmail.com>
2022-01-22 22:23:46 -0300

    Proyecto Viajes Chile Terminado

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    let buttons = document.querySelectorAll('a');
+    buttons.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        e.preventDefault();
+        let target = e.currentTarget.getAttribute('href');
+        
+        document.querySelector(target).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  
+  })
